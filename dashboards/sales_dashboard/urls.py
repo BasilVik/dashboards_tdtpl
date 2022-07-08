73f3bff188c4beee8963c31a7cdaf03d01773059
vasiliy <basilvik>
2022-07-08 11:34:57 +0700

    вывел таблицу на страницу

--- dashboards/sales_dashboard/urls.py
+++ dashboards/sales_dashboard/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'sales_dashboard'
 
 urlpatterns = [
-    # Главная страница
+    path('', views.index, name='index'),
     path('sales_dashboard/', views.sales_dashboard, name='sales_dashboard'),
 ] 
--- dashboards/sales_dashboard/views.py
+++ dashboards/sales_dashboard/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+import csv
 
-
-# Главная страница
-def sales_dashboard(request):    
+def index(request):
     template = 'sales_dashboard/index.html'
-    title = 'Данные по продажам'
-    text = 'Текст страницы'
+    title = 'Главная страница'
     context = {
         'title': title,
-        'text': text,
     }
     return render(request, template, context)
+
+def sales_dashboard(request):    
+    template = 'sales_dashboard/sales_dashboard.html'
+    title = 'Данные по продажам'
+
+    with open("/home/vasiliy/Dev/dashboards_tdtpl/dashboards/sales_dashboard/data/test.csv", encoding='utf-8') as r_file:
+        fieldnames = ['year', 'month_number', 'month', 'sales']
+        file_data = csv.DictReader(r_file, delimiter = "|", fieldnames = fieldnames)
+
+        context = {
+            'title': title,
+            'file_data': file_data,
+        }
+        return render(request, template, context)
