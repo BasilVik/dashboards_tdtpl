2c18c2a0ea6d076d3c3ca48ddf5ce369fd111afd
vasiliy <basilvik>
2022-07-16 17:51:33 +0700

    добавил новые таблицы

--- dashboards/sales_dashboard/urls.py
+++ dashboards/sales_dashboard/urls.py
@@ -6,4 +6,7 @@ app_name = 'sales_dashboard'
 urlpatterns = [
     path('', views.index, name='index'),
     path('sales_dashboard/', views.sales_dashboard, name='sales_dashboard'),
+    path('bank_dashboard/', views.bank_dashboard, name='bank_dashboard'),
+    path('account_total_dashboard/', views.account_total_dashboard, name='account_total_dashboard'),
+    path('account_dashboard/', views.account_dashboard, name='account_dashboard'),
 ] 
--- dashboards/sales_dashboard/views.py
+++ dashboards/sales_dashboard/views.py
@@ -13,8 +13,50 @@ def sales_dashboard(request):
     template = 'sales_dashboard/sales_dashboard.html'
     title = 'Данные по продажам'
 
-    with open("/home/vasiliy/Dev/dashboards_tdtpl/dashboards/sales_dashboard/data/test.csv", encoding='utf-8') as r_file:
+    with open("/home/vasiliy/Dev/dashboards_tdtpl/dashboards/sales_dashboard/data/sales.csv", encoding='utf-8') as r_file:
         fieldnames = ['year', 'month_number', 'month', 'sales']
+        file_data = csv.DictReader(r_file, delimiter = "|", fieldnames = fieldnames)
+
+        context = {
+            'title': title,
+            'file_data': file_data,
+        }
+        return render(request, template, context)
+
+def bank_dashboard(request):
+    template = 'sales_dashboard/bank_dashboard.html'
+    title = 'Данные по счетам'
+
+    with open("/home/vasiliy/Dev/dashboards_tdtpl/dashboards/sales_dashboard/data/bank.csv", encoding='utf-8') as r_file:
+        fieldnames = ['organization', 'bank', 'amount']
+        file_data = csv.DictReader(r_file, delimiter = "|", fieldnames = fieldnames)
+
+        context = {
+            'title': title,
+            'file_data': file_data,
+        }
+        return render(request, template, context)
+
+def account_total_dashboard(request):
+    template = 'sales_dashboard/account_total_dashboard.html'
+    title = 'Данные по взаиморасчетам общие'
+
+    with open("/home/vasiliy/Dev/dashboards_tdtpl/dashboards/sales_dashboard/data/acc_total.csv", encoding='utf-8') as r_file:
+        fieldnames = ['enterprise_debt', 'partner_debt']
+        file_data = csv.DictReader(r_file, delimiter = "|", fieldnames = fieldnames)
+
+        context = {
+            'title': title,
+            'file_data': file_data,
+        }
+        return render(request, template, context)
+
+def account_dashboard(request):
+    template = 'sales_dashboard/account_dashboard.html'
+    title = 'Данные по взаиморасчетам'
+
+    with open("/home/vasiliy/Dev/dashboards_tdtpl/dashboards/sales_dashboard/data/acc.csv", encoding='utf-8') as r_file:
+        fieldnames = ['partner', 'enterprise_debt', 'partner_debt']
         file_data = csv.DictReader(r_file, delimiter = "|", fieldnames = fieldnames)
 
         context = {
